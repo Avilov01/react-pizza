3f1bde884912fb1a36d76bde74092bdc2100fce6
Avilov01 <Avilov01@gmail.com>
2021-08-19 13:55:12 +0300

    add library Classnames, create components: Button, Categories, Header

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classNames from 'classnames';
+
+export default function Button({className, outline, children}){
+	return (
+		<button className={classNames('button', className, { 'button--outline': outline })}>
+			{children}
+		</button>
+	);
+};
---
+++ src/components/Categories.jsx
@@ -0,0 +1,28 @@
+import classNames from 'classnames';
+import React, { useState } from 'react';
+
+export default function Categories({ items }) {
+	const [activeItem, setActiveItem] = useState('Все');
+
+	const onSelectItem = name => {
+		setActiveItem(name);
+	};
+
+	const isActiveItem = name => (activeItem === name ? true : false);
+
+	return (
+		<div className='categories'>
+			<ul>
+				{items &&
+					items.map(name => (
+						<li
+							onClick={() => onSelectItem(name)}
+							key={name}
+							className={classNames({ active: isActiveItem(name) })}>
+							{name}
+						</li>
+					))}
+			</ul>
+		</div>
+	);
+}
