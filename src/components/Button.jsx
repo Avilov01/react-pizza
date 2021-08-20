841326a3dd29aff186072a44102dc91afd32aacc
Avilov01 <Avilov01@gmail.com>
2021-08-20 20:25:41 +0300

    add react-router & axios, create component PizzaBlock

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import classNames from 'classnames';
 
-export default function Button({className, outline, children}){
+export default function Button({ className, outline, children }) {
 	return (
 		<button className={classNames('button', className, { 'button--outline': outline })}>
 			{children}
 		</button>
 	);
-};
+}
